8574cd6af1f77ef1bd61bd2650392d86b7b09dd9
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-02-02 10:54:15 +0100

    worked on mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
+"use client";
 import Image from "next/image";
 import logo from "../assets/logo.png";
-import { BsGrid3X3GapFill } from 'react-icons/bs';
+import { HiMenu } from "react-icons/hi";
+import Link from "next/link";
+import { useState } from "react";
 export default function Navbar() {
+  const [bool, setbool] = useState<boolean>(false);
   return (
     <div>
-      <div className=" container p flex justify-between items-center mx-auto">
+      <div className=" container p lg:flex hidden justify-between items-center mx-auto">
         <Image src={logo} alt="logo" className="object-contain" />
         {/* <img src={logo} alt="" /> */}
-        <div className="gap-x-14 lg:flex hidden">
+        <div className="gap-x-14 flex ">
           <button className="bg-[#4A9EED] text-white rounded-[10px] font-bold  px-8 py-2">
             REGISTER AS A CLEANER
           </button>
@@ -20,9 +24,35 @@ export default function Navbar() {
         </div>
         {/* <div className="w-full"></div> */}
       </div>
-      <div className=" fixed top-0  px-2 xss:px-4 xs:px-6 sm:px-10 md:px-8  font-poppins w-full bg-[#87ACA3]/[0.4] lg:hidden   py-5 flex  z-[70] rounded-b-lg justify-between">
-        
+      <div className=" fixed top-0  px-2  sm:px-10 md:px-8   w-full  lg:hidden   py-4 flex  z-[70] rounded-b-sm justify-between">
+        <Link href="">
+          {" "}
+          <Image src={logo} alt="" className="md:w-40 w-24 " />
+        </Link>
+        <span className="flex  items-center gap-x-2">
+          <button className="bg-[#4A9EED] text-sm text-white rounded-[10px] font-bold  px-3 py-2">
+            REGISTER AS A CLEANER
+          </button>
+          <HiMenu
+            className="text-black font-bold text-xl  sm:text-3xl md:text-4xl"
+            onClick={() => setbool(!bool)}
+          />
+        </span>
+      </div>
+      {bool && (
+        <div
+          className="h-[7rem] lg:hidden w-full flex flex-col shadow text-base rounded-b-xl items-center justify-evenly text-white  bg-[#4A9EED]
+         text-light z-50 fixed top-[7.5rem] "
+        >
+          <Link href="" onClick={() => setbool(false)}>
+            {" "}
+            LOG IN
+          </Link>
+          <Link href="" onClick={() => setbool(false)}>
+            MENU
+          </Link>
         </div>
+      )}
     </div>
   );
 }
