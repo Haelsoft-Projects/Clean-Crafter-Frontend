3665048c2a2d7ee27c76a2b63563aaa117ddb1d7
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-02-02 14:49:20 +0100

    working on about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,13 +4,25 @@ import img2 from "../assets/landingpage/sub2.png";
 export default function About() {
   return (
     <div>
-      <div className="gap-y-6 flex flex-col container mx-auto relative mb-32">
-        <Image src={img2} alt="A man cleaning " className="ab" />
-        <Image
-          src={img1}
-          alt="A woman cleaning "
-          className="absolute top-64 left-64"
-        />
+      <div className=" flex items-center  container justify-between mx-auto mt-10 mb-32">
+        <div className=" w-1/2 ">
+          <Image
+            src={img2}
+            alt="A man cleaning "
+            className="object-contain w-[40rem] "
+          />
+          <Image
+            src={img1}
+            alt="A woman cleaning  "
+            className="-mt-32 ml-[18rem]  object-contain w-[26rem]"
+          />
+        </div>
+        <div className="flex h-full w-[30%] ">
+          <div className="flex items-center">
+            <span className=" h-2 w-12 bg-[#4A9EED] self-start"></span>
+            <span>About CleanCrafters</span>
+          </div>
+        </div>
       </div>
     </div>
   );
